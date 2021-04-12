dacdcc4da0c542fcdc8a2bbad0ea3b2ccbd6fbaa
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-04-12 10:08:41 +0000

    added sites list to JS instead of keeping data on the HTML document

--- src/script.js
+++ src/script.js
@@ -1,13 +1,35 @@
+const sites = [
+  {
+    url: 'https://app.diagrams.net/',
+    title: 'draw.io',
+    category: 'drawing'
+  }
+];
+
+function displaySiteList(sites) {
+  $('#links').empty();
+
+  sites.forEach(site => 
+    $('#links').append(
+      $('<li>').append(
+        $('<a>')
+          .attr('href', site.url)
+          .text(site.title)
+      )
+    )
+  );
+
+};
+
 $(_ => {
-  $('#form').submit(_ => {
-    const 
-      url = $('#url').val(),
-      title = $('#title').val(),
-      category = $('#category').val();
+  $('#form').submit(event => {
+    event.preventDefault();
+    sites.push({
+      url: $('#url').val(),
+      title: $('#title').val(),
+      category: $('#category').val(),
+    });
 
-    $('#links')
-      .append(`<li><a href="${url}">${title}</a></li>`);
-
-    return false;
+    displaySiteList(sites);
   });
 });
