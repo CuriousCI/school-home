ee65b918c3ae65e366f1d9a71d585ea37a804a16
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-04-24 06:35:16 +0000

    first commit

--- src/script.js
+++ src/script.js
@@ -1,35 +1,51 @@
-const sites = [
-  {
-    url: 'https://app.diagrams.net/',
-    title: 'draw.io',
-    category: 'design'
-  }
-];
+let sites = [];
 
-function updateSiteList(list, sites) {
-  $(list).empty();
+function updateSiteList(listSelector, sites) {
+  $(listSelector).empty();
 
   sites.forEach(site => 
-    $(list).append(
-      $('<li>').append(
-        $('<a>')
-          .attr('href', site.url)
-          .text(site.title)
-      )
+    $(listSelector).append(
+      $('<li>')
+        .append(
+          $('<a>')
+            .attr('href', site.url)
+            .text(site.title)
+        )
+        .append(
+          $('<img>')
+            .attr('src', 'assets/delete.png')
+            .attr('alt', 'trash icon')
+            .data('id', site.id)
+            .addClass('icon')
+        )
     )
   );
 
 };
 
-$(_ => 
+
+$(_ => {
   $('#form').submit(event => {
     event.preventDefault();
+
     sites.push({
+      id: Math.floor(Math.random() * 100000),
       url: $('#url').val(),
       title: $('#title').val(),
       category: $('#category').val()
     });
 
     updateSiteList('#links', sites);
+
+    $('#form').each(function() {
+      this.reset();
+    });
   })
-);
+
+  $('#links').on('click', '.icon', function() {
+    const id = $(this).data('id');
+    sites = sites.filter(site => site.id != id);
+    
+    updateSiteList('#links', sites);
+  });
+});
